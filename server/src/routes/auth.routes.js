9a41f1e408c6b8d9ed674d336b22b9d12413c4dd
s-f-malot <s0583232209@gmail.com>
2026-05-28 18:49:13 +0300

    fix: removing double files, feat: adding post method to users and refresh route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.post("/register", (req, res) => {
   res.send("auth: register");
 });
-
+router.post("/refresh", (req, res) => {
+  res.send("auth: refresh token");
+});
 router.post("/login", (req, res) => {
   res.send("auth: login");
 });

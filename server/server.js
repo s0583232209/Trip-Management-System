b48b9988765ea19660d5bb47b9578f122afb3d8b
s0583232209 <s0583232209@gmail.com>
2026-06-08 13:43:40 +0300

    fix(Trips): adding emegencies functionalty after checks

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ import { configDotenv } from "dotenv";
 import cors from "cors";
 import authRouter from "./src/routes/auth.routes.js";
 import verifyToken from "./src/middlewares/auth.middleware.js";
-// import log from "./utils/logger.js";
 import tripsRouter from "./src/routes/trips.routes.js";
 import filesRouter from "./src/routes/files.routes.js";
 import mediaRouter from "./src/routes/media.routes.js";
 import emergencyRouter from "./src/routes/emergencies.routes.js";
 import logger from "./src/middlewares/logger.middleware.js";
-import usersRouter from "./src/routes/users.routes.js"
+import usersRouter from "./src/routes/users.routes.js";
 import cookieParser from "cookie-parser";
 configDotenv();
 
@@ -20,7 +19,7 @@ const HOST = process.env.HOST || "localhost";
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
-app.use("/api", verifyToken)
+app.use("/api", verifyToken);
 app.use("/api", logger);
 // app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
